a573ef526bb767835e033d171ef42641e41a328f
VedantPatle0305 <vedantpatle594@gmail.com>
2025-08-16 16:18:56 +0530

    Initial Commit

---
+++ Veather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Veather
+//
+//  Created by Vedant Patle on 16/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Veather/VeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeatherApp.swift
+//  Veather
+//
+//  Created by Vedant Patle on 16/08/25.
+//
+
+import SwiftUI
+
+@main
+struct VeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
