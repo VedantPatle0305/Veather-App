5677607125a646c75bbbe53f26b95571f148eb87
VedantPatle0305 <vedantpatle594@gmail.com>
2025-08-17 13:44:49 +0530

    first commit

---
+++ Veather/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Veather
+//
+//  Created by Vedant Patle on 16/08/25.
+//
+
+import Foundation
---
+++ Veather/WeatherDataViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherDataViewModel.swift
+//  Veather
+//
+//  Created by Vedant Patle on 16/08/25.
+//
+
+import Foundation
